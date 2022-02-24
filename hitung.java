c0ff2458741c01d492bae73edf561527b241a1bd
BryanVirasantaka <100253980+BryanVirasantaka@users.noreply.github.com>
2022-02-24 10:19:31 +0700

    Add files via upload

---
+++ hitung.java
@@ -0,0 +1,18 @@
+
+
+package overload;
+
+/**
+ *
+ * @author Bryan
+ */
+public class hitung {
+  public static void main(String[] args) {
+        BangunDatar hitunglah=new BangunDatar();
+        
+        hitunglah.luas(2, 5, 6);
+        hitunglah.luas(3.5, 6.9);
+    
+    }
+    
+}
